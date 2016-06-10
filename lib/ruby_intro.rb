785e6fe280e98b40e18c2149d5a15b75b2b9cd35
Sebastian <sebasvelez@gmail.com>
2016-06-10 11:49:22 -0500

    initial commit

---
+++ lib/ruby_intro.rb
@@ -0,0 +1,54 @@
+# When done, submit this entire file to the autograder.
+
+# Part 1
+
+def sum arr
+  return 0 unless arr.size > 0
+  arr.inject(:+)
+end
+
+def max_2_sum arr
+  return 0 unless arr.size > 0
+  return arr[0] if arr.size == 1
+
+  arr.sort[-2..-1].inject(:+)
+end
+
+def sum_to_n? arr, n
+  return false if arr.empty?
+  arr.permutation(2).any? { |a,b| a + b == n }
+end
+
+# Part 2
+
+def hello(name)
+  "Hello, #{name}"
+end
+
+def starts_with_consonant? s
+  /^[^aeiou\W]/i.match s
+end
+
+def binary_multiple_of_4? s
+  return false unless /^\d+$/.match s
+  (s.to_i 2) % 4 == 0 ? true : false
+end
+
+# Part 3
+
+class BookInStock
+
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty?
+    raise ArgumentError if price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$#{sprintf '%.2f', price}"
+  end
+end
